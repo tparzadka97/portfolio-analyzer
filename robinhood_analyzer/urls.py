350ddc1525dc012017711da370456d1b4ef25b1d
Tomasz <tparzadk@purdue.edu>
2021-03-13 11:39:24 -0600

    Changed navbar behavior

--- robinhood_analyzer/urls.py
+++ robinhood_analyzer/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
